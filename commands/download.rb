d624231753252e403e0bd2bb675c0f1886026737
Chew <chew@chew.pw>
2020-04-04 13:34:50 -0500

    fix var names 2: electric boogaloo

--- commands/download.rb
+++ commands/download.rb
@@ -4,11 +4,11 @@ module Download
   command(:download) do |event|
     tardis_site = RestClient.get('http://tardisjenkins.duckdns.org:8080/job/TARDIS/lastSuccessfulBuild/')
     tardis_parsed = Nokogiri::HTML.parse(tardis_site.body)
-    tardis_build = doc.at('#breadcrumbs > li:nth-child(5) > a').text
+    tardis_build = tardis_parsed.at('#breadcrumbs > li:nth-child(5) > a').text
 
     tardis_chunk_site = RestClient.get('http://tardisjenkins.duckdns.org:8080/view/TARDIS/job/TARDISChunkGenerator/lastSuccessfulBuild/')
     tardis_chunk_parsed = Nokogiri::HTML.parse(tardis_chunk_site.body)
-    tardis_chunk_build = doc.at('#breadcrumbs > li:nth-child(7) > a').text
+    tardis_chunk_build = tardis_chunk_parsed.at('#breadcrumbs > li:nth-child(7) > a').text
     event.channel.send_embed do |embed|
       embed.title "Handles Help"
       embed.description = "Make sure your TARDIS is up to date! If your server version is 1.15.2 or greater, it's ideal to make sure your TARDIS is up to date as well. Use the links below to download the latest build of BOTH TARDIS and TARDISChunkGenerator"
