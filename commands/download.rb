3905c0c7ffa955d70d4d6cd1a48e4d2df8797a5e
Chew <chew@chew.pw>
2020-04-04 13:35:21 -0500

    forgot the equals sign

--- commands/download.rb
+++ commands/download.rb
@@ -10,7 +10,7 @@ module Download
     tardis_chunk_parsed = Nokogiri::HTML.parse(tardis_chunk_site.body)
     tardis_chunk_build = tardis_chunk_parsed.at('#breadcrumbs > li:nth-child(7) > a').text
     event.channel.send_embed do |embed|
-      embed.title "Handles Help"
+      embed.title = "Handles Help"
       embed.description = "Make sure your TARDIS is up to date! If your server version is 1.15.2 or greater, it's ideal to make sure your TARDIS is up to date as well. Use the links below to download the latest build of BOTH TARDIS and TARDISChunkGenerator"
       embed.add_field(name: 'TARDIS', value: "[Build #{tardis_build}](http://tardisjenkins.duckdns.org:8080/job/TARDIS/lastSuccessfulBuild)", inline: true)
       embed.add_field(name: 'TARDISChunkGenerator', value: "[Build #{tardis_chunk_build}](http://tardisjenkins.duckdns.org:8080/view/TARDIS/job/TARDISChunkGenerator/lastSuccessfulBuild)", inline: true)
