b2e9533f7871a4d813540a45c2bbd62cccb5a2f9
Chew <chew@chew.pw>
2023-09-29 13:58:20 -0500

    improved wiki command

--- commands/wiki.rb
+++ commands/wiki.rb
@@ -8,20 +8,26 @@ module Wiki
       embed.description = 'Searching the wiki...'
     end
 
-    wiki_site = RestClient.get('https://eccentricdevotion.github.io/TARDIS/site-map.html')
-    wiki_parsed = Nokogiri::HTML.parse(wiki_site.body)
+    wiki_site = RestClient.get('https://tardis.pages.dev/search-doc.json')
+    wiki_parsed = JSON.parse wiki_site
 
-    length = wiki_parsed.search("a").length
+    length = wiki_parsed.length
 
     results = []
 
-    wiki_parsed.search('a')[4..length - 3].each do |item|
-      if item.text.downcase.include?(search.downcase)
-        results.push "[#{item.text}](https://eccentricdevotion.github.io/TARDIS/#{item.attributes['href'].value})"
+    wiki_parsed.each do |item|
+      if item['title'].downcase.include?(search.downcase)
+        if item['type'] == 0
+          results.push "[#{item['title']}](https://tardis.pages.dev#{item['url']})"
+        elsif item['type'] == 1
+          results.push "[#{item['pageTitle']} > #{item['title']}](https://tardis.pages.dev#{item['url']})"
+        end
       end
     end
 
     results.push "No results found" if results.empty?
+
+    results = results.sort
 
     embed = Discordrb::Webhooks::Embed.new(
       title: 'Handles Wiki Search',
