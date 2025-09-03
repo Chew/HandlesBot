0543c10d8a384d440eb88f099f0b40d70d8efba3
Chew <chew@chew.pw>
2025-09-02 19:40:59 -0500

    remove build checking

--- bot.rb
+++ bot.rb
@@ -2,9 +2,6 @@ require 'discordrb'
 require 'yaml'
 require 'json'
 require 'rest-client'
-require 'rufus-scheduler'
-
-SCHEDULER = Rufus::Scheduler.new
 
 STARTTIME = Time.now
 
@@ -31,10 +28,6 @@ def loadpls
   end
 end
 
-SCHEDULER.every('2m') do
-  check_for_build
-end
-
 def last_build(update = nil)
   if update
     # update the last build number
@@ -48,90 +41,7 @@ def last_build(update = nil)
   end
 end
 
-def check_for_build
-  puts "Checking for new builds..."
-  begin
-    begin
-      latest_in_server = last_build
-    rescue StandardError
-      return
-    end
-
-    tardis_site = JSON.parse(RestClient.get('http://tardisjenkins.duckdns.org:8080/job/TARDIS/lastSuccessfulBuild/api/json/'))
-    latest_on_jenkins = tardis_site['number'].to_i
-
-    if latest_on_jenkins != latest_in_server
-      changes = tardis_site['changeSet']['items']
-                  .map { |e| "[#{e['commitId'][0..7]}](https://github.com/eccentricdevotion/TARDIS/commit/#{e['commitId']}) - #{e['msg'].chomp}" }
-                  .map { |e| "* #{e}" }
-      build_time = tardis_site['timestamp'] / 1000
-      puts "New build found!"
-
-      payload = {
-        "components": [
-          {
-            "type": 17,
-            "components": [
-              {
-                "type": 10,
-                "content": "# TARDIS Plugin Update",
-              },
-              {
-                "type": 10,
-                "content": "Build #{latest_on_jenkins} is now available!\nReleased <t:#{build_time}:R> (<t:#{build_time}:f>)",
-              },
-              {
-                "type": 14,
-                "divider": true
-              },
-              {
-                "type": 10,
-                "content": changes.join("\n")
-              },
-            ]
-          },
-          {
-            "type": 1,
-            "components": [
-              {
-                "type": 2,
-                "label": "View on Jenkins",
-                "style": 5,
-                "url": tardis_site['url']
-              }
-            ]
-          }
-        ],
-        "flags": 1 << 15,
-      }
-
-      puts "Sending..."
-      message = RestClient.post("https://discord.com/api/v10/channels/696383624563392552/messages", payload.to_json, Authorization: BOT.token, 'Content-Type': :json)
-      #puts "Message sent: #{message}"
-      m_id = JSON.parse(message)['id']
-      puts "Message ID: #{m_id}"
-      begin
-        RestClient.post("https://discord.com/api/v6/channels/696383624563392552/messages/#{m_id}/crosspost", nil, Authorization: BOT.token)
-      rescue RestClient::Unauthorized => e
-        puts "Not authorized to crosspost: #{e}"
-        return
-      end
-
-      last_build(latest_on_jenkins)
-    else
-      puts "Up to date!"
-    end
-  rescue RestClient::BadRequest => e
-    puts "Bad request: #{e}"
-    puts e.response.body
-  rescue StandardError
-    puts "Error checking for build: #{$!}"
-    return
-  end
-end
-
 loadpls
-check_for_build
 
 BOT.command(:reload) do |event|
   break unless event.user.id == CONFIG['owner_id']
